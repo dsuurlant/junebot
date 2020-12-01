7384401a02b20f5344ddaed3d95cb7cf1c4f5006
Daniëlle Suurlant <danielle.suurlant@gmail.com>
2020-12-01 18:23:35 +0100

    Add cat api info, restructure if-tree for commands

--- src/server.js
+++ src/server.js
@@ -3,20 +3,37 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// Bot basic configuration
+const invokes = ['june', 'June', 'j!'];
+
 client.once('ready', () => {
 	console.log("Ready!");
 });
 
 client.on('message', msg => {
-	if (msg.content.startsWith('june help') || msg.content.startsWith('j!help') || msg.content.startsWith('June help')) {
-		msg.reply("I don't have a lot of options yet. Here they are:");
-		const help = new Discord.MessageEmbed()
-			.setTitle("June's commands")
-			.addField('Addressing June', 'You can ask June by typing "june", "June" or "j!".')
-			.addField("`j!help`, `June help`", "Ask for help (this command!)");
-		msg.channel.send(help);
-	} else if (msg.content.startsWith('june') || msg.content.startsWith('j!') || msg.content.startsWith('June')) {
-		msg.reply("I'm still under construction, but thanks for talking to me!");
+	let invoked = false
+	for (const invoke in invokes) {
+		if (msg.content.startsWith(invoke)) {
+			invoked = true;
+			break;
+		}
+	}
+
+	if (invoked) {
+		if (msg.content.endsWith('help')) {
+			msg.reply("I don't have a lot of options yet. Here they are:");
+			const help = new Discord.MessageEmbed()
+				.setTitle("June's commands")
+				.addField('Addressing June', 'You can ask June by typing "june", "June" or "j!".')
+				.addField("`j!help`, `June help`", "Ask for help (this command!)");
+			msg.channel.send(help);
+		} else if (msg.content.endsWith('cat')) {
+			// load cat
+		} else if (msg.content.endsWith('dog')) {
+			// load dog
+		} else {
+			msg.reply("I'm still under construction, but thanks for talking to me!");
+		}
 	}
 });
 
