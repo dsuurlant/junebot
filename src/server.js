681a3b5fdd36a1243fe277775d213ef84e35ba10
Daniëlle Suurlant <danielle.suurlant@gmail.com>
2020-12-01 14:18:10 +0100

    Add basic help command

--- src/server.js
+++ src/server.js
@@ -8,8 +8,15 @@ client.once('ready', () => {
 });
 
 client.on('message', msg => {
-	if (msg.content === 'ping') {
-		msg.reply('Pong!');
+	if (msg.content.startsWith('june help') || msg.content.startsWith('j!help') || msg.content.startsWith('June help')) {
+		msg.reply("I don't have a lot of options yet. Here they are:");
+		const help = new Discord.MessageEmbed()
+			.setTitle("June's commands")
+			.addField('Addressing June', 'You can ask June by typing "june", "June" or "j!".')
+			.addField("`j!help`, `June help`", "Ask for help (this command!)");
+		msg.channel.send(help);
+	} else if (msg.content.startsWith('june') || msg.content.startsWith('j!') || msg.content.startsWith('June')) {
+		msg.reply("I'm still under construction, but thanks for talking to me!");
 	}
 });
 
